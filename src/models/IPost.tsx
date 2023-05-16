658813fd12d569d13ae0e88b120a43a5432eeea8
Bifard-Ios <Bifard-Ios@mail.ru>
2023-05-16 15:37:08 +0700

    created setField function

--- src/models/IPost.tsx
+++ src/models/IPost.tsx
@@ -3,3 +3,16 @@ export interface IPost {
   title: string;
   body: string;
 }
+
+const POST: IPost = {
+  id: 1,
+  title: "title",
+  body: "body",
+};
+type SetField = <T extends keyof IPost>(obj:IPost, payload:{key:T, value:IPost[T]})=>void | any
+
+const setField:SetField = (obj, payload)=> {
+  const newPost: IPost = { ...obj, [payload.key]: payload.value };
+  console.log(newPost);
+}
+setField(POST, {key:'id', value:1})
